9e61b632e2a724826059cfbe7deca63e04b2351e
Azka <muhammadazkanurazhim@gmail.com>
2026-04-20 20:54:45 +0700

    add utils

---
+++ src/utils/prisma.ts
@@ -0,0 +1,5 @@
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "../generated/prisma/client.js";
+
+const adapter = new PrismaBetterSqlite3({ url: "file:./prisma/db.dev" })
+export const prisma = new PrismaClient({ adapter })
