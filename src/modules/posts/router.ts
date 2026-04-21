02ed09e64a0dce1ebee62580000d8505c2a11da3
Azka <muhammadazkanurazhim@gmail.com>
2026-04-21 09:01:30 +0700

    update endpoints in blog post router

--- src/modules/posts/router.ts
+++ src/modules/posts/router.ts
@@ -1,20 +1,66 @@
 import { Hono } from "hono";
 import { prisma } from "../../prisma.js"
+import { zValidator } from "@hono/zod-validator";
+import { createPostSchema, updatePostSchema } from "./schema.js";
 
 export const postRouter = new Hono()
   .get("/", async (c) => {
     const posts = await prisma.post.findMany()
     return c.json(posts);
   })
-  .get("/:id", (c) => {
-    return c.json({});
+  .get("/:id", async (c) => {
+    const id = c.req.param("id")
+    const posts = await prisma.post.findUnique({
+      where: {
+        id: Number(id)
+      }
+    })
+
+    if(!posts){
+      return c.json({ message: "Blog Post not found" }, 404)
+    }
+
+    return c.json(posts);
   })
-  .post("/", (c) => {
-    return c.json({ message: "" }, 201);
+  .post("/", zValidator("json", createPostSchema), async (c) => {
+    const body = c.req.valid("json")
+    const newPost = await prisma.post.create({
+      data: {
+        title: body.title,
+        content: body.content
+      }
+    })
+    return c.json(newPost, 201);
   })
-  .patch("/:id", (c) => {
-    return c.json({});
+  .patch("/:id", zValidator("json", updatePostSchema), async (c) => {
+    const id = c.req.param("id")
+    const body = c.req.valid("json")
+
+    const updatePost = await prisma.post.update({
+      where: {
+        id: Number(id)
+      },
+      data: {
+        title: body.title,
+        content: body.content
+      }
+    })
+
+    return c.json(updatePost);
   })
-//   .delete("/:id", (c) => {
-//     return c.json({ message: "" });
-//   });
+  .delete("/:id", (c) => {
+    return c.json({ message: "Blog Post deleted successfully" })
+  })
+  .post("/:id/mark-as-published", async (c) => {
+    const id = c.req.param("id")
+    const updatePost = await prisma.post.update({
+      where: {
+        id: Number(id)
+      },
+      data: {
+        publish: true
+      }
+    })
+    
+    return c.json(updatePost)
+  })
